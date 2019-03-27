99884da260b17bc8b8a2e077846d746bc45fed9a
Amar Nath Yogi <immortal2907@gmail.com>
2019-03-27 19:10:27 +0530

    List & Detail Views half way

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
 from catalog.models import Book, Author, BookInstance, Genre
 
@@ -31,3 +31,10 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
