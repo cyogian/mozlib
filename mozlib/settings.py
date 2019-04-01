4a47ec6835221162cfc833b271eb63ea28117350
Amar Nath Yogi <immortal2907@gmail.com>
2019-04-01 19:00:18 +0530

    Deployment

--- mozlib/settings.py
+++ mozlib/settings.py
@@ -20,10 +20,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_w4pi=8btg3p%f4w**rv+2#@v&+pjnxtswltf2s!5h9qnv5y_g'
+# SECRET_KEY = '_w4pi=8btg3p%f4w**rv+2#@v&+pjnxtswltf2s!5h9qnv5y_g'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '_w4pi=8btg3p%f4w**rv+2#@v&+pjnxtswltf2s!5h9qnv5y_g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
